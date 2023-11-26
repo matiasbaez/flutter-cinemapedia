c32ab9a3c81f0a18e24374386d7fb2522e157ce4
Matias Baez <matiasbaez2512@gmail.com>
2023-11-26 14:28:06 -0300

    Configure dotenv and riverpod

---
+++ lib/config/constants/environment.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+
+  static String theMovieDBKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'INVALID_KEY_THE_MOVIE_DB';
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/config/config.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+
+  await dotenv.load(fileName: '.env');
+
+  runApp(
+    const ProviderScope(
+      child: MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
